61b8ba3ef8364be05c650a2213c4e8d0cb920a21
Kenol David <kenoldavid.git@gmail.com>
2019-06-24 01:11:43 -0400

    changed font-family [issue 11]

--- octobercms/config/database.php
+++ octobercms/config/database.php
@@ -57,7 +57,7 @@ return [
             'host'      => 'localhost',
             'port'      => 3306,
             'database'  => 'kenoldavid',
-            'username'  => 'admin',
+            'username'  => 'octobercms',
             'password'  => 'password',
             'charset'   => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
